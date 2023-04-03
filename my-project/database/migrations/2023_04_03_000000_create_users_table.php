92d26a8716404991f9b701aab1b6fd598a1ff219
gawach <gawa9.1998@gmail.com>
2023-04-04 00:03:26 +0900

    migration_file_delete_unneeded_create_needed

--- my-project/database/migrations/2023_04_03_000000_create_users_table.php
+++ my-project/database/migrations/2023_04_03_000000_create_users_table.php
@@ -13,12 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable(false);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('password')->nullable(false);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
